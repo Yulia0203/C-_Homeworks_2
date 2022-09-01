9d2dd064a675739495bda5578154bcfddf5dd079
Yulia <mamaeva.jula@gmail.com>
2022-09-01 21:56:33 +0300

    Дз2_Урок4_Задача27

---
+++ Project002_Seminar4/Program.cs
@@ -0,0 +1,33 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+System.Console.WriteLine("Введите число N: ");
+bool isNumber = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isNumber)
+{
+    System.Console.WriteLine("Это не число");
+    return;
+}
+
+int SumNumber(int numberN)
+{    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(n);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+
