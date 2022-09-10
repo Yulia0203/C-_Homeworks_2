2b060e890ef110f9451020abf674b6ce4d021cc1
Yulia <mamaeva.jula@gmail.com>
2022-09-10 23:40:59 +0300

    Дз1_Урок7_Задача47

---
+++ Project001_Seminar7/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (m == 0)
+{
+    Console.WriteLine("Некорректное число");
+    return;
+}
+
+Console.WriteLine("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 0)
+{
+    Console.WriteLine("Некорректное число");
+    return;
+}
+
+
+double[,] array = new double[m, n];
+CreateArrayDouble(array);
+WriteArray(array);
+Console.WriteLine();
+
+void CreateArrayDouble(double[,] array)
+{
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      array[i, j] = new Random().NextDouble() * 20 - 10;
+    }
+  }
+}
+
+void WriteArray (double[,] array)
+{
+for (int i = 0; i < m; i++)
+  {
+      for (int j = 0; j < n; j++)
+      {
+        double alignNumber = Math.Round(array[i, j], 1);
+        Console.Write(alignNumber + " ");
+      }
+      Console.WriteLine();
+  }
+}
