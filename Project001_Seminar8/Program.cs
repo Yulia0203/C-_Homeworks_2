0a6a8a2e8e7e7df247c828682b41ec530f878a86
Yulia <mamaeva.jula@gmail.com>
2022-09-15 15:19:12 +0300

    Дз1_Урок8_Задача54

---
+++ Project001_Seminar8/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void GetRandomDoubleArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GetSortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int minPosition = j;
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, k] > array[i, minPosition]) minPosition = k;
+            }
+            int temporary = array[i, j];
+            array[i, j] = array[i, minPosition];
+            array[i, minPosition] = temporary;
+
+        }
+    }
+    return array;
+}
+
+int[,] array = new int[3, 4];
+GetRandomDoubleArray(array);
+PrintArray(array);
+Console.WriteLine();
+int[,] newArray = GetSortArray(array);
+PrintArray(newArray);
