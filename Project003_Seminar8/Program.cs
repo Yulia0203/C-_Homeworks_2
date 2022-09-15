fe98f0f29d5232ebd3b94fcba8e18f757aee67c9
Yulia <mamaeva.jula@gmail.com>
2022-09-15 16:42:16 +0300

    Дз3_Урок8_Задача58

---
+++ Project003_Seminar8/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+void GetRandomDoubleArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintDoubleArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GetMatrixMultiplication(int[,] matrix1, int[,] matrix2)
+{
+    int[,] matrix3 = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    for (int i = 0; i < matrix3.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix2.GetLength(0); k++)
+            {
+                matrix3[i, j] += matrix1[i, k] * matrix2[k, j];
+            }
+        }
+    }
+    return matrix3;
+}
+
+int[,] matrix1 = new int[2, 2];
+int[,] matrix2 = new int[2, 3];
+
+
+GetRandomDoubleArray(matrix1);
+PrintDoubleArray(matrix1);
+Console.WriteLine();
+GetRandomDoubleArray(matrix2);
+PrintDoubleArray(matrix2);
+
+Console.WriteLine();
+PrintDoubleArray(GetMatrixMultiplication(matrix1, matrix2));
